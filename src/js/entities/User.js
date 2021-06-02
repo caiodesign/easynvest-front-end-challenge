bceac67541556d514f8ddcc541905548be1681fd
Caio Oliveira <colive.dev@gmail.com>
2021-06-02 10:54:08 -0300

    chore: remove useless id from user object

--- src/js/entities/User.js
+++ src/js/entities/User.js
@@ -1,16 +1,10 @@
-import { ID } from '../lib/id';
 
 class User {
   constructor(props) {
-    this.id = props.id;
     this.name = props.name;
     this.email = props.email;
     this.cpf = props.cpf;
     this.phone = props.phone;
-
-    if (!props.id) {
-      this.id = ID();
-    }
   }
 }
 
--- src/js/lib/id.js
+++
@@ -1,5 +0,0 @@
-export function ID () {
-  return '_' + Math.random().toString(36).substr(2, 9);
-}
-
-export default ID;
