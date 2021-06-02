911ef5b7166519134485919071842157853d3721
Caio Oliveira <colive.dev@gmail.com>
2021-06-02 14:18:36 -0300

    chore: remove useless files

--- src/js/global.js
+++
@@ -1,9 +0,0 @@
-import { GlobalOnLoadListener } from './listeners/implementations/GlobalListeners/GlobalOnLoadListener';
-
-function init () {
-  const globalOnLoadListener = new GlobalOnLoadListener(document.querySelector('#loader'));
-
-  globalOnLoadListener.init();
-}
-
-init();
