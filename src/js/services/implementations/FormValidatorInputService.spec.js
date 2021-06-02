cc26be46cb81ce8c2317f41548cafff74a2dd70d
Caio Oliveira <colive.dev@gmail.com>
2021-06-02 14:25:37 -0300

    feat: add jest

---
+++ src/js/services/implementations/FormValidatorInputService.spec.js
@@ -0,0 +1,78 @@
+import { FormValidatorInputService } from '.';
+
+const user = {
+  name: 'Caio Oliveira',
+  email: 'colive.dev@gmail.com',
+  cpf: '420.739.308-08',
+  phone: '(11) 96913-2927',
+};
+
+const formValidatorInputService = new FormValidatorInputService();
+
+describe('index.html', () => {
+  it('should return TRUE when user name has 3 or more characters', () => {
+    expect(formValidatorInputService.validateName(user.name)).toBeTruthy();
+  });
+
+  it('should return FALSE when user name does not have 3 or more characters', () => {
+    expect(formValidatorInputService.validateName('c')).toBeFalsy();
+    expect(formValidatorInputService.validateName('ca')).toBeFalsy();
+    expect(formValidatorInputService.validateName('cai')).toBeTruthy();
+    expect(formValidatorInputService.validateName('caio')).toBeTruthy();
+  });
+
+  it('should return TRUE when EMAIL is valid', () => {
+    expect(formValidatorInputService.validateEmail(user.email)).toBeTruthy();
+  });
+
+  it('should return FALSE when EMAIL is not valid', () => {
+    const emails = ['caio', 'caio.', 'caio.com', '@.com'];
+
+    emails.map((email) => {
+      expect(formValidatorInputService.validateEmail(email)).toBeFalsy();
+    });
+  });
+
+  it('should return TRUE when CPF is valid', () => {
+    expect(formValidatorInputService.validateCpf(user.cpf)).toBeTruthy();
+  });
+
+  it('should return FALSE when CPF is not valid', () => {
+    const cpf = [
+      '11111111111',
+      '22222222222',
+      '33333333333',
+      '44444444444',
+      '55555555555',
+      '66666666666',
+      '77777777777',
+      '88888888888',
+      '99999999999',
+    ];
+
+    cpf.map((number) => {
+      expect(formValidatorInputService.validateCpf(number)).toBeFalsy();
+    });
+  });
+
+  it('should return TRUE when phone is valid', () => {
+    expect(formValidatorInputService.validatePhone(user.phone)).toBeTruthy();
+  });
+
+  it('should return FALSE when phone is not valid', () => {
+    const numbers = [
+      '1',
+      '11',
+      '111',
+      '1111',
+      '11111',
+      '111111',
+      '2222222',
+      '33333333',
+    ];
+
+    numbers.map((number) => {
+      expect(formValidatorInputService.validatePhone(number)).toBeFalsy();
+    });
+  });
+});
